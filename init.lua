a91b9e0037a7fc51894acd239e4e85bf0a56884c
Josemaria <marinmjosemaria@gmail.com>
2023-03-14 11:34:37 -0400

    t

---
+++ init.lua
@@ -0,0 +1,12 @@
+vim.opt.relativenumber = true
+vim.opt.number = true
+vim.opt.autoindent = true
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.autoindent = true
+vim.opt.mouse = 'a'
+vim.opt.softtabstop = 4
+vim.opt.smarttab = true
+
+require('plugins')
+require('keymaps')
