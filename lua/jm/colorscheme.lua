f61b57a16b557d965acc7936fdd2c0644c143334
Josemaria <marinmjosemaria@gmail.com>
2023-03-15 03:06:32 -0400

    Treesitter added

--- lua/jm/colorscheme.lua
+++ lua/jm/colorscheme.lua
@@ -1,7 +1,16 @@
+require("tokyonight").setup({ 
+    -- disable italic
+  styles = {
+    comments = { italic = false },
+    keywords = { italic = false },
+    functions = {},
+    variables = {},
+  },
+})
+
 local colorscheme = "tokyonight"
 
 local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
 if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
   return
 end
